83c30731b63e3d857b0fdcf376dfac0ca937a220
Raphaël Benzazon <r.benzazon@webpublication.fr>
2019-05-22 02:06:30 +0300

    -added google analytics to the router and configured ddns

--- frontend/personal-media/src/App.js
+++ frontend/personal-media/src/App.js
@@ -5,34 +5,79 @@ import { Route,Switch } from "react-router-dom";
 import {PlaylistProvider } from './PlaylistContext';
 import APAppBar from './APAppBar';
 import PLAppBar from './PLAppBar';
+import createHistory from 'history/createBrowserHistory'
+import ReactGA from 'react-ga';
+import { createMuiTheme,MuiThemeProvider } from '@material-ui/core/styles';
+import { Router} from "react-router-dom";
 
-function App(props){
+ReactGA.initialize('UA-52487002-2');
 
-  return (
+const history = createHistory();
+history.listen(location => {
+	ReactGA.set({ page: location.pathname })
+	ReactGA.pageview(location.pathname);
+})
+
+
+const theme = createMuiTheme({
+    typography: {
+      useNextVariants: true,
+    },
+    palette: {
+        type: 'dark',
+        background:{
+            paper:'#303030',
+            default:'#212121',
+        },
+        primary:{
+            dark:'#baa0ea',
+            main:'#baa0ea',
+            contrastText:'#e0e0e0',
+            light:'#e0e0e0',
+        },
+        secondary:{
+            dark:'#ce9178',
+            main:'#baa0ea',
+            contrastText:'#e0e0e0',
+            light:'#e0e0e0',
+        }
+    },
+});
+
+export default class App extends React.Component{
+  componentDidMount() {
+		ReactGA.pageview(window.location.pathname)
+	}
+  
+  render(){
+    return (
+    <MuiThemeProvider theme={theme}>
+      <Router history={history} >
         <PlaylistProvider >
-        <React.Fragment>
-          <PLAppBar />
-          <Switch>
-          <Route exact path='/' render={(props) => <HomeLayout {...props} />} />
-          <Route exact strict path={[
-            "/folder",
-            "/folder/",
-            '/search/:searchKeyword',
-            '/favorite',
-            '/playlist/:playlistName',
-            '/artist/:artistName',
-            '/album/:albumName',
-            '/genre/:genreName'
-            ]} render={(props) => <FolderLayout {...props} />} />
-          
-          <Route exact strict path='/folder/*' render={(props) => <FolderLayout {...props} />} />
-          </Switch>
-          <APAppBar />
-        </React.Fragment>
+          <React.Fragment>
+            <PLAppBar />
+            <Switch>
+            <Route exact path='/' render={(props) => <HomeLayout {...props} />} />
+            <Route exact strict path={[
+              "/folder",
+              "/folder/",
+              '/search/:searchKeyword',
+              '/favorite',
+              '/playlist/:playlistName',
+              '/artist/:artistName',
+              '/album/:albumName',
+              '/genre/:genreName'
+              ]} render={(props) => <FolderLayout {...props} />} />
+            
+            <Route exact strict path='/folder/*' render={(props) => <FolderLayout {...props} />} />
+            </Switch>
+            <APAppBar />
+          </React.Fragment>
         </PlaylistProvider>
-        );
+      </Router>
+    </MuiThemeProvider>
+    )
+  }
   
 }
-
-export default App;
 
--- frontend/personal-media/src/index.js
+++ frontend/personal-media/src/index.js
@@ -3,40 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router} from "react-router-dom";
-import { createMuiTheme,MuiThemeProvider } from '@material-ui/core/styles';
 
-const theme = createMuiTheme({
-    typography: {
-      useNextVariants: true,
-    },
-    palette: {
-        type: 'dark',
-        background:{
-            paper:'#303030',
-            default:'#212121',
-        },
-        primary:{
-            dark:'#baa0ea',
-            main:'#baa0ea',
-            contrastText:'#e0e0e0',
-            light:'#e0e0e0',
-        },
-        secondary:{
-            dark:'#ce9178',
-            main:'#baa0ea',
-            contrastText:'#e0e0e0',
-            light:'#e0e0e0',
-        }
-    },
-});
+
 
 ReactDOM.render(
-<MuiThemeProvider theme={theme}>
-    <Router>
         <App />
-    </Router>
-</MuiThemeProvider>, document.getElementById('root'));
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
