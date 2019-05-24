f42b40f98101c95df2dae0cbdac13142c0585ff4
Raphaël Benzazon <r.benzazon@webpublication.fr>
2019-05-24 13:32:02 +0300

    -added redirect for front end routes to the express prod server (backend/server.js)

--- backend/server.js
+++ backend/server.js
@@ -3,5 +3,26 @@ const app = express()
 /*app.get("/", function(req, res) {
     res.send("Hello World")
 })*/
-app.use(express.static('public'))
+app.get('', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/artist*', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/album*', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/search*', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/playlist*', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/folder*', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.get('/genre*', function(request, response){    
+    response.sendFile(__dirname + '/public/index.html');
+});
+app.use(express.static('public'));
 app.listen(3000);
