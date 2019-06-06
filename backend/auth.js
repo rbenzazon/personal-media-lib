444f97ab6667654cbfc4b552cc73387f1e3bb45c
Raphaël Benzazon <r.benzazon@webpublication.fr>
2019-06-06 12:46:03 +0300

    adding login logs

--- backend/auth.js
+++ backend/auth.js
@@ -41,14 +41,16 @@ router.post('/register', verify, async (req,res)=>{
 });
 router.post('/login', async (req,res)=>{
     //validate
+    console.log("login attempt");
     const {error} = loginValidation(req.body);
     if(error){
         return res.status(400).send(error.details[0].message);
     }
     //checking if existing user
+    console.log("searching user "+req.body.email);
     const user = await User.findOne({email:req.body.email});
     if(!user) return res.status(400).send({message:"email or password invalid"});
-
+    console.log("comparing pass");
     const validPass = await bcrypt.compare(req.body.password,user.password);
     if(!validPass) return res.status(400).send({message:"email or password invalid"});
     
