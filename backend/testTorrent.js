fdf718dc7f10408140b372caad94ca803b4ce82e
Raphaël Benzazon <r.benzazon@webpublication.fr>
2019-06-15 13:44:41 +0300

    1

--- backend/testTorrent.js
+++ backend/testTorrent.js
@@ -5,7 +5,7 @@ while(randomName.length < 7){
 }
 const url = "magnet:?xt=urn:btih:0783de8676b4beab459650a1ff443e48331c3e50&dn=Weezer+-+Island+In+The+Sun+%28Version+1%29&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fexodus.desync.com%3A6969";
 async function run(){
-    const child = spawn('aria2c', ['"'+url+'"','--dir=/home/rbenzazon/Music/'+randomName]);
+    const child = spawn('aria2c', ['"'+url+'"','--dir=/home/rbenzazon/Music/'+randomName],{ shell: true });
 
     child.on('exit', code => {
         console.log(`Exit code is: ${code}`);
